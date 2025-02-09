242882a01a08fb56215a6302434d9b08a79bd7fa
jimfeserHTW <s0596491@htw-berlin.de>
2025-02-10 00:05:52 +0100

    Hardcoded Elemts

--- src/ChemicalElement.java
+++ src/ChemicalElement.java
@@ -11,6 +11,7 @@ public class ChemicalElement {
     private final int boilingPoint;
     private final int[] oxidationNums;
     private final double standardElectrodePotential;
+    private final boolean extra;
 
     public ChemicalElement(int num, String letter, String[] elementNames, double weight, double negativity, String eConfig, int melt, int boil, int[] oxidation, double electrodePotential){
         this.atomicNumber = num;
@@ -21,7 +22,63 @@ public class ChemicalElement {
         this.electronConfig = eConfig;
         this.meltingPoint = melt;
         this.boilingPoint = boil;
-        this.oxidationNums =oxidation;
+        this.oxidationNums = oxidation;
         this.standardElectrodePotential = electrodePotential;
+        this.extra = false;
+    }
+    public ChemicalElement(int num, String letter, String[] elementNames, double weight, String eConfig, int melt, int boil, int[] oxidation, double electrodePotential){
+        this.atomicNumber = num;
+        this.symbol = letter;
+        this.names = elementNames;
+        this.atomicWeight = weight;
+        this.electronegativity = -1000;
+        this.electronConfig = eConfig;
+        this.meltingPoint = melt;
+        this.boilingPoint = boil;
+        this.oxidationNums = oxidation;
+        this.standardElectrodePotential = electrodePotential;
+        this.extra = false;
+    }
+
+    public ChemicalElement(int num, String letter, String[] elementNames, double weight, String eConfig, int melt, int boil){
+        this.atomicNumber = num;
+        this.symbol = letter;
+        this.names = elementNames;
+        this.atomicWeight = weight;
+        this.electronegativity = -1000;
+        this.electronConfig = eConfig;
+        this.meltingPoint = melt;
+        this.boilingPoint = boil;
+        this.oxidationNums = new int[] {-1000};
+        this.standardElectrodePotential = -1000;
+        this.extra = false;
+    }
+
+    public ChemicalElement(int num, String letter, String[] elementNames, double weight, double negativity, String eConfig, int melt, int boil, int[] oxidation, double electrodePotential, boolean xtra){
+        this.atomicNumber = num;
+        this.symbol = letter;
+        this.names = elementNames;
+        this.atomicWeight = weight;
+        this.electronegativity = negativity;
+        this.electronConfig = eConfig;
+        this.meltingPoint = melt;
+        this.boilingPoint = boil;
+        this.oxidationNums = oxidation;
+        this.standardElectrodePotential = electrodePotential;
+        this.extra = xtra;
+    }
+
+    public ChemicalElement(int num, String letter, String[] elementNames, double weight, double negativity, String eConfig, int[] oxidation, double electrodePotential, boolean xtra){
+        this.atomicNumber = num;
+        this.symbol = letter;
+        this.names = elementNames;
+        this.atomicWeight = weight;
+        this.electronegativity = negativity;
+        this.electronConfig = eConfig;
+        this.meltingPoint = -1000;
+        this.boilingPoint = -1000;
+        this.oxidationNums = oxidation;
+        this.standardElectrodePotential = electrodePotential;
+        this.extra = xtra;
     }
 }
